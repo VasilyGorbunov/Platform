84bcc78ddf44f69ea24b0457e1891eb64deb609c
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-12-13 18:35:41 +0300

    305

---
+++ Platform/Middleware.cs
@@ -0,0 +1,27 @@
+﻿namespace Platform
+{
+    public class QueryStringMiddleware
+    {
+        private RequestDelegate _next;
+
+        public QueryStringMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+            {
+                if(!context.Response.HasStarted)
+                {
+                    context.Response.ContentType = "text/plain";
+                }
+                
+                await context.Response.WriteAsync("Class middleware \n");
+            }
+
+            await _next(context);
+        }
+    }
+}
--- Platform/Program.cs
+++ Platform/Program.cs
@@ -1,5 +1,54 @@
+using Platform;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+
+//app.Use(async (context, next) =>
+//{
+//    await next();
+//    await context.Response.WriteAsync($"\nStatusCode: {context.Response.StatusCode}");
+//});
+
+//app.Use(async (context, next) =>
+//{
+//    if (context.Request.Path == "/short")
+//    {
+//        await context.Response.WriteAsync($"Request Short Circuited");
+//    } 
+//    else
+//    {
+//        await next();
+//    }
+//});
+
+//app.Use(async (context, next) =>
+//{
+//    if(context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+//    {
+//        context.Response.ContentType = "text/plain";
+//        await context.Response.WriteAsync("Custom middleware \n");
+//    }
+
+//    await next();
+//});
+
+app.Map("/branch", branch =>
+{
+    branch.UseMiddleware<QueryStringMiddleware>();
+
+    branch.Run(async (context) =>
+    {
+        await context.Response.WriteAsync($"Branch Middleware");
+    });
+
+    //branch.Use(async (HttpContext context, Func<Task> next) =>
+    //{
+    //    await context.Response.WriteAsync($"Branch Middleware");
+    //});
+});
+
+app.UseMiddleware<QueryStringMiddleware>();
 
 app.MapGet("/", () => "Hello World!");
 
